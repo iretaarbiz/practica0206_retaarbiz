6ab21b9cc33d169f03d41d2e186733b58a69d1c2
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-12-13 09:28:26 +0100

    Ejercicio 6
    Mejoras del ejercicio 6 para que el programa tenga mayor navegabilidad y evitar que el programa termine abruptamente

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -16,47 +16,91 @@ Terminar el programa.
 diccionario = {}
 terminar = True
 while terminar == True:
-    print("Ingresa el número correspondiente a la acción que quieres realizar: \n")
+    print("Ingresa el número correspondiente a la acción que quieres realizar:")
     print("(1) Añadir alumno/a, (2) Eliminar alumno/a, (3) Mostrar alumno/a, (4) Listar todo el alumnado, (5) Listar alumnado aprobado, (6) Terminar")
-    accion = int(input(""))
-    if accion == 1:
-        diccionarioalumnos = {}
-        nif = input("Introduce el NIF del alumno: ")
-        print("Introduce el nombre, apellidos, teléfono, correo y si esta aprobado o no el alumno.")
-        for i in range(5):
-            if i == 0:
-                nombre= input("Introduce el nombre: ")
-                diccionarioalumnos["nombre"] = nombre
-            elif i == 1:
-                apellidos = input("Introduce los apellidos: ")
-                diccionarioalumnos["apellidos"] = apellidos
-            elif i == 2:
-                telefono = input("Introduce el teléfono: ")
-                diccionarioalumnos["teléfono"] = telefono
-            elif i == 3:
-                correo = input("Introduce el correo: ")
-                diccionarioalumnos["correo"] = correo
-            else:
-                aprobado = input("Introduce si esta aprobado o no (Si/No): ")
-                if aprobado == "Si" or "si":
-                    aprobadobool = True
-                elif aprobado == "No" or "no":
-                    aprobadobool = False
-                diccionarioalumnos["aprobado"] = aprobadobool
-        diccionario[nif] = diccionarioalumnos
-    elif accion == 2:
-        nif = input("Introduce el NIF del alumno: ")
-        del diccionario[nif]
-        print("El alumno con NIF", nif, "ha sido eliminado")
-    elif accion == 3:
-        nif = input("Introduce el NIF del alumno: ")
-        print(diccionario[nif])
-    elif accion == 4:
-        for i in diccionario.keys():
-            print(diccionario[i]['nombre'])
-    elif accion == 5:
-        for i in diccionario.keys():
-            if diccionario[i]['aprobado'] == True:
-                print(diccionario[i]['nombre'])   
-    elif accion == 6:
-        terminar = False
+    accion = input("")
+    if accion == "1" or accion == "2" or accion == "3" or accion == "4" or accion == "5" or accion == "6":
+        accion = int(accion)
+        if accion == 1:
+            print("Has accedido a la opción de 'Añadir alumno/a'")
+            diccionarioalumnos = {}
+            nif = input("Introduce el NIF del alumno: ")
+            print("Introduce el nombre, apellidos, teléfono, correo y si esta aprobado o no el alumno.")
+            nombre= input("Introduce el nombre: ")
+            diccionarioalumnos["nombre"] = nombre
+            apellidos = input("Introduce los apellidos: ")
+            diccionarioalumnos["apellidos"] = apellidos
+            telefono = input("Introduce el teléfono: ")
+            diccionarioalumnos["teléfono"] = telefono
+            correo = input("Introduce el correo: ")
+            diccionarioalumnos["correo"] = correo
+            aprobado = input("Introduce si esta aprobado o no (Si/No): ")
+            if aprobado == "Si" or "si":
+                aprobadobool = True
+            elif aprobado == "No" or "no":
+                aprobadobool = False
+            diccionarioalumnos["aprobado"] = aprobadobool
+            diccionario[nif] = diccionarioalumnos
+        elif accion == 2:
+            print("Has accedido a la opción de 'Eliminar alumno/a'")
+            while True:
+                if len(diccionario) == 0:
+                    print("La base de datos esta vacia, prueba a usar la acción 'Añadir alumno/a' antes de usar las demas acciones")
+                    print("Volviendo al menú principal \n")
+                    break
+                else:
+                    nif = input("Introduce el NIF del alumno o 'salir' para volver al menú principal: ")
+                    if nif in diccionario.keys():
+                        del diccionario[nif]
+                        print("El alumno con NIF", nif, "ha sido eliminado")
+                        break
+                    elif nif == "salir":
+                        break
+                    else:
+                        print("El NIF introducido no está en nuestra base de datos")
+        elif accion == 3:
+            print("Has accedido a la opción de 'Mostrar alumno/a'")
+            while True:
+                if len(diccionario) == 0:
+                    print("La base de datos esta vacia, prueba a usar la acción 'Añadir alumno/a' antes de usar las demas acciones")
+                    print("Volviendo al menú principal \n")
+                    break
+                    nif = input("Introduce el NIF del alumno o 'salir' para volver al menú principal: ")
+                    if nif in diccionario.keys():
+                        print(diccionario[nif])
+                        break
+                    elif nif == "salir":
+                        break
+                    else:
+                        print("El NIF introducido no está en nuestra base de datos")
+        elif accion == 4:
+            print("Has accedido a la opción de 'Listar todo el alumnado'")
+            if len(diccionario) == 0:
+                    print("La base de datos esta vacia, prueba a usar la acción 'Añadir alumno/a' antes de usar las demas acciones")
+                    print("Volviendo al menú principal \n")
+            for i in diccionario.keys():
+                print(diccionario[i]['nombre'])
+        elif accion == 5:
+            print("Has accedido a la opción de 'Listar alumnado aprobado'")
+            if len(diccionario) == 0:
+                    print("La base de datos esta vacia, prueba a usar la acción 'Añadir alumno/a' antes de usar las demas acciones")
+                    print("Volviendo al menú principal \n")
+            for i in diccionario.keys():
+                if diccionario[i]['aprobado'] == True:
+                    print(diccionario[i]['nombre'])   
+        elif accion == 6:
+            print("¿Estás seguro de que quieres terminar la ejecución del programa? Todos los datos se borrarán (Si/No)")
+            while True:
+                acabar = input("")
+                if acabar.lower() == "si":
+                    print("El programa ha terminado")
+                    terminar = False
+                    break
+                elif acabar.lower() == "no":
+                    print("Volviendo al menú principal")
+                    break
+                else:
+                    print("La palabra introducida es diferente a Si y No, vuelve a introducir la palabra")
+
+    else:
+        print("Porfavor introduce un número asociado a una función del programa \n")
